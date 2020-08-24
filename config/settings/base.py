bf9c64125ebe403b89b9d7b20a3b7ff434560625
rngustj9139 <mnb9139@daum.net>
2020-08-24 14:50:11 +0900

    separate settings.py in config/settings/base.py, local.py, prod.py for ALLOWED_HOST

--- config/settings/base.py
+++ config/settings/base.py
@@ -14,8 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
